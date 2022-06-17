01c935c0297d21c467d3309543fe88c73376f974
Shouryamup <73136513+Shouryamup@users.noreply.github.com>
2022-06-17 15:48:05 +0530

    After adding comments in code

--- challenge-1/js/functions.js
+++ challenge-1/js/functions.js
@@ -6,38 +6,41 @@ let secLeft=0,
     timerStart=0,
     timerOn=null;
  
- 
+// timer update updates the value in the input field for every second
 const timerupdate = () => {
     timerStart++;
     secLeft = Math.floor((timerEnd - timerStart) % 60);
     minLeft = Math.floor((timerEnd - timerStart) / 60);
     sec.value = secLeft.toString().length == 2 ? secLeft : `0${secLeft}`;
     min.value = minLeft.toString().length == 2 ? minLeft : `0${minLeft}`;
+    //if the timer ends then setinterval ends, alert is popped and ring color is changed
     if (timerStart == timerEnd) {
             clearInterval(timerOn);
             timerOn = null;
             timerStart = 0;
             startButton.textContent ='start';
             document.querySelector(".ring").classList.add("ending");
-            // if (sec.value===0){
                 alert(" TIMER ENDED!!!");
-            // }
           }
 }
 
 const  starttimer = () => {
     if (!timerOn) {
+        //set inetval makes the timerupdate run for every second so it can update time
         timerOn = setInterval(timerupdate, 1000);
       }
       else {
+        // if timer is already on, then then this function pauses the timer
         clearInterval(timerOn);
         timerOn=null;   
     }
 };
 
+//function to make timerOn variable null
 const make_timerOnNull = ()=>{
     timerOn=null;
 }
+//function to make timerstart zero
 const make_timerStart_startover = ()=>{
     timerStart=0;
 }
--- challenge-1/js/index.js
+++ challenge-1/js/index.js
@@ -1,21 +1,24 @@
 import { timerupdate ,starttimer,timerOn,make_timerOnNull,make_timerStart_startover} from "./functions.js";
-
+//xonstant variable initialisation
 const startButton = document.querySelector(".start");
 const settingButton = document.querySelector(".settings");
-
+//Variable initialisation
 let min = document.querySelector(".minutes input"),
     sec = document.querySelector(".seconds input"),
     timerEnd=parseInt(min.value) * 60 + parseInt(sec.value),
     togglesettings=false;
-
+// creating event for click on start button
 startButton.addEventListener("click",function togglestart(){
         togglesettings=false;
+        //making them disabled so that values cannot be changed anymore
         min.disabled=true;
         sec.disabled=true;
+        // calculating total value of time according the values in input field of minutes and second
         timerEnd=parseInt(min.value) * 60 + parseInt(sec.value)
-    
         if(startButton.textContent ==='stop'){
+            //changing text of the button to start
             startButton.textContent ='start';
+            //start timer function starts the timer
             starttimer();
         }
         else{
@@ -23,11 +26,14 @@ startButton.addEventListener("click",function togglestart(){
             alert("Enter correct value");
            }
             else if (!(parseInt(min.value) === 0 && parseInt(sec.value) === 0)) {
-                startButton.textContent ='stop';  
+                //changing text of the button to stop
+                startButton.textContent ='stop';
+                //remove ending class from the ring to change the color back to green  
                 document.querySelector(".ring").classList.remove("ending")
                 starttimer();
             }
             else{
+                //alert if no value is enetered in the timer
                 alert("Enter Time Value in the Timer!");
             };
     
@@ -35,14 +41,18 @@ startButton.addEventListener("click",function togglestart(){
 })
     
 settingButton.addEventListener("click",function togglesetting(){
+    //allow setting to only work if timer is not in progress
     if(!timerOn){
+    //allow user to edit values in the input field and fetch the values
      togglesettings=true;
      min.disabled=false;
      sec.disabled=false;
      min.value= document.querySelector(".minutes input").value;
      sec.value= document.querySelector(".seconds input").value;
      clearInterval(timerOn);
+     //function to make timerOn variable null
      make_timerOnNull(); 
+     //function to make timerstart zero
      make_timerStart_startover();}
 })
 
